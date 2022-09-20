a522db5092bb0c1bfdccc7f5569cd662153fd7ef
Gabriel Pajot <github@gab.les-cactus.co>
2022-09-20 16:23:46 +0200

    feat: add logging

--- zenconfig/write.py
+++ zenconfig/write.py
@@ -1,9 +1,12 @@
+import logging
 import sys
 from abc import ABC
 from typing import ClassVar, Dict
 
 from zenconfig.base import ZenConfigError
 from zenconfig.read import ReadOnlyConfig
+
+logger = logging.getLogger(__name__)
 
 
 class Config(ReadOnlyConfig, ABC):
@@ -21,8 +24,16 @@ class Config(ReadOnlyConfig, ABC):
             )
         path = paths[0]
         if not path.exists():
+            logger.debug("creating file at path %s", path)
             path.touch(mode=self.FILE_MODE)
-        self._format().dump(path, self._schema().to_dict(self))
+        fmt = self._format()
+        logger.debug(
+            "using %s to save %s to %s",
+            fmt.__class__.__name__,
+            self.__class__.__name__,
+            path,
+        )
+        fmt.dump(path, self._schema().to_dict(self))
 
     def clear(self) -> None:
         """Delete the config file(s)."""
@@ -30,4 +41,5 @@ class Config(ReadOnlyConfig, ABC):
         if sys.version_info[:2] != (3, 7):
             kwargs["missing_ok"] = True
         for path in self._paths():
+            logger.debug("deleting file at path %s", path)
             path.unlink(**kwargs)
