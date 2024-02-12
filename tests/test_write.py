5482f1bfc493060d58dc578ab702e4de9c0337e1
Gabriel Pajot <github@gab.les-cactus.co>
2024-02-12 17:48:03 +0100

    chore(format): rerun

--- tests/test_write.py
+++ tests/test_write.py
@@ -8,7 +8,8 @@ from zenconfig.write import Config
 
 class TestReadOnlyConfig:
     def test_should_not_be_able_to_write_to_multiple_files(self, mocker):
-        class Cfg(Config): ...
+        class Cfg(Config):
+            ...
 
         mocker.patch.object(Cfg, "_paths", return_value=(Path(), Path()))
 
